4c54d94115b4ff0dac26149564ede0e3baba7e58
SOUMEN MITRA <soumen8492@gmail.com>
2025-01-23 19:08:38 +0530

    Reward entity added

---
+++ src/main/java/CustomerRewardsApp/models/Reward.java
@@ -0,0 +1,49 @@
+package CustomerRewardsApp.models;
+
+public class Reward {
+    private String custId;
+    private String month;
+    private int point;
+
+    public String getCustId() {
+        return custId;
+    }
+
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public Reward() {
+    }
+
+    public Reward(String custId, String month, int point) {
+        this.custId = custId;
+        this.month = month;
+        this.point = point;
+    }
+
+    @Override
+    public String toString() {
+        return "Reward{" +
+                "custId='" + custId + '\'' +
+                ", month='" + month + '\'' +
+                ", point=" + point +
+                '}';
+    }
+}
