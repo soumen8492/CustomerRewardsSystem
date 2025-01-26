8a5f539d25df6408f497380c2284f09c37cd147f
SOUMEN MITRA <soumen8492@gmail.com>
2025-01-26 12:26:08 +0530

    Repository Testcases added

--- src/test/java/CustomerRewardsApp/customerRewardsRepository/RewardsRepositoryTest.java
+++ src/test/java/CustomerRewardsApp/customerRewardsRepository/RewardsRepositoryTest.java
@@ -1,16 +1,30 @@
 package CustomerRewardsApp.customerRewardsRepository;
 
+import CustomerRewardsApp.models.Transaction;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@DataJpaTest
 class RewardsRepositoryTest {
+
+    @Autowired
+    RewardsRepository rewardsRepository;
 
     @Test
     void getTransactionByCustId() {
+        assertEquals(18,rewardsRepository.getTransactionByCustId("1").size());
     }
 
     @Test
     void getTransactionByCustIdAndMonth() {
+        assertEquals(8,rewardsRepository.getTransactionByCustIdAndMonth("1","jan").size());
     }
 }
