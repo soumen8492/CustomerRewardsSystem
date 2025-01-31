23069ae1956a9eef3ee0afa269fa4e1b1533b0f1
SOUMEN MITRA <soumen8492@gmail.com>
2025-01-31 21:58:13 +0530

    changed save method name

--- src/main/java/CustomerRewardsApp/CustomerRewardsSystemApplication.java
+++ src/main/java/CustomerRewardsApp/CustomerRewardsSystemApplication.java
@@ -22,6 +22,6 @@ public class CustomerRewardsSystemApplication implements CommandLineRunner {
       */
     @Override
     public void run(String... args) throws Exception {
-        rewardsService.setRewardPoints();
+        rewardsService.createRewardPoints();
     }
 }
