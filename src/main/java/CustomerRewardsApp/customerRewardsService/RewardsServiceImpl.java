a259d1239fef17bf4aeeca10dcb70dd30fa88216
SOUMEN MITRA <soumen8492@gmail.com>
2025-01-31 21:57:36 +0530

    Service and controller updated for better JSON structure

--- src/main/java/CustomerRewardsApp/customerRewardsService/RewardsServiceImpl.java
+++ src/main/java/CustomerRewardsApp/customerRewardsService/RewardsServiceImpl.java
@@ -3,10 +3,7 @@ package CustomerRewardsApp.customerRewardsService;
 import CustomerRewardsApp.customerRewardsException.CustomerIdNotFoundException;
 import CustomerRewardsApp.customerRewardsRepository.RewardsRepository;
 import CustomerRewardsApp.customerRewardsRepository.TransactionRepository;
-import CustomerRewardsApp.models.Reward;
-import CustomerRewardsApp.models.RewardDetail;
-import CustomerRewardsApp.models.RewardResponse;
-import CustomerRewardsApp.models.Transaction;
+import CustomerRewardsApp.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,95 +20,78 @@ public class RewardsServiceImpl implements RewardsService {
     @Autowired
     TransactionRepository transactionRepository;
 
-    public void setRewardPoints() {
+    public void createRewardPoints()
+    {
         List<Transaction> transactions = transactionRepository.findAll();
-        Map<String, HashMap<String, Reward>> rewardMap = new HashMap<>();
-        //Mapping and Grouping Rewards by CustId and month of transaction
-        for (Transaction t : transactions) {
-            HashMap<String, Reward> monthlyMap;
-            String month = LocalDate.parse(t.getTranDate(), DateTimeFormatter.
-                            ofPattern("MM/dd/yyyy")).
-                    format(DateTimeFormatter.
-                            ofPattern("MMMM", Locale.ENGLISH));
-            Reward r;
-            if (!rewardMap.containsKey(t.getCustId())) {
-                monthlyMap = new HashMap<>();
-                monthlyMap.put(month, new Reward((t.getCustId() + ":" + month), t.getCustId(), t.getName(), month, computeRewardPoint(t.getAmount()), t.getAmount()));
-                rewardMap.put(t.getCustId(), monthlyMap);
-            } else {
-                monthlyMap = rewardMap.get(t.getCustId());
-                if (monthlyMap.containsKey(month)) {
-                    r = monthlyMap.get(month);
-                    r.setPoints(r.getPoints() + computeRewardPoint(t.getAmount()));
-                    r.setAmountSpent(r.getAmountSpent()+t.getAmount());
-                } else {
-                    r = new Reward((t.getCustId() + ":" + month), t.getCustId(), t.getName(), month, computeRewardPoint(t.getAmount()),t.getAmount());
-                }
-                monthlyMap.put(month, r);
-            }
-            rewardMap.put(t.getCustId(), monthlyMap);
+        List<Reward> rewards = new ArrayList<>();
+        for(Transaction transaction : transactions)
+        {
+            rewards.add(new Reward(transaction.getTransactionId(), transaction.getTransactionId(), computeRewardPoint(transaction.getAmount())));
         }
-        //Converting the group to a Reward List object
-        List<Reward> rewardList = new ArrayList<>();
-        for (String custId : rewardMap.keySet()) {
-            rewardList.addAll(rewardMap.get(custId).values());
-        }
-        //save the data
-        rewardsRepository.saveAll(rewardList);
+        rewardsRepository.saveAll(rewards);
     }
-
     @Override
-    public ResponseEntity<List<RewardResponse>> createRewardResponse() {
-        RewardResponse rewardResponse = new RewardResponse();
+    public ResponseEntity<List<RewardResponse>> getRewardResponse() {
+        List<String> CustIdList = transactionRepository.findCustId();
         List<RewardResponse> rewardResponseList = new ArrayList<>();
-        List<Reward> rewardList = new ArrayList<>();
-        List<String> custIdList = new ArrayList<>();
-        custIdList = rewardsRepository.findCustId();
-        int totalRewards = 0;
-        int amountSpent = 0;
-        for (String id : custIdList) {
-            rewardList = rewardsRepository.findByCustId(id);
-            rewardResponse.setName(rewardList.get(0).getName());
-            rewardResponse.setCustId(id);
-            List<RewardDetail> rewardDetails = new ArrayList<>();
-            for (Reward reward : rewardList) {
-                rewardDetails.add(new RewardDetail(reward.getMonth(), reward.getPoints(), reward.getAmountSpent()));
-                totalRewards += reward.getPoints();
-                amountSpent += reward.getAmountSpent();
-            }
-            Collections.sort(rewardDetails,(i,j)-> monthSort(i.getMonth())>monthSort(j.getMonth())?1:-1);
-            rewardResponse.setTotal_points(totalRewards);
-            rewardResponse.setRewardDetails(rewardDetails);
-            rewardResponse.setTotalAmountSpent(amountSpent);
-            rewardResponseList.add(rewardResponse);
-            totalRewards = 0;
-            amountSpent = 0;
-            rewardResponse = new RewardResponse();
+        for(String custId : CustIdList)
+        {
+            List<Transaction> transactionsByCustId = transactionRepository.findTransactionsByCustId(custId);
+            List<Reward> rewardsByCustId = rewardsRepository.findRewardsByCustId(custId);
+            rewardResponseList.add(createRewardResponse(custId, transactionsByCustId, rewardsByCustId));
         }
         return new ResponseEntity<>(rewardResponseList, HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<RewardResponse> createRewardResponse(String custId) {
-        List<Reward> rewards = rewardsRepository.findByCustId(custId);
-        if (rewards.size() == 0)
-            throw new CustomerIdNotFoundException("Customer Id " + custId + " not present in DB", "Id does not exist");
+    public ResponseEntity<RewardResponse> getRewardResponse(String custId) {
+        List<Transaction> transactionsByCustId = transactionRepository.findTransactionsByCustId(custId);
+        List<Reward> rewardsByCustId = rewardsRepository.findRewardsByCustId(custId);
+        if(transactionsByCustId.size()==0 || rewardsByCustId.size()==0) throw new CustomerIdNotFoundException("Customer Id "+ custId+" not in DB", "Customer ID does not exist");
+        return new ResponseEntity<>(createRewardResponse(custId, transactionsByCustId,rewardsByCustId), HttpStatus.OK);
+    }
+
+    private RewardResponse createRewardResponse(String custId, List<Transaction> transactionsByCustId, List<Reward> rewardsByCustId) {
         RewardResponse rewardResponse = new RewardResponse();
         rewardResponse.setCustId(custId);
-        rewardResponse.setName(rewards.get(0).getName());
+        rewardResponse.setName(transactionsByCustId.get(0).getName());
         List<RewardDetail> rewardDetails = new ArrayList<>();
-        int totalPoints = 0;
-        int totalAmountSpent = 0;
-        for (Reward r : rewards) {
-            rewardDetails.add(new RewardDetail(r.getMonth(), r.getPoints(), r.getAmountSpent()));
-            totalPoints += r.getPoints();
-            totalAmountSpent += r.getAmountSpent();
+        HashMap<String, RewardDetail> monthMap = new HashMap<>();
+        for(int i=0; i<transactionsByCustId.size();i++)
+        {
+            Transaction tran = transactionsByCustId.get(i);
+            String month = LocalDate.parse(tran.getTranDate(), DateTimeFormatter.
+                            ofPattern("MM/dd/yyyy")).
+                    format(DateTimeFormatter.
+                            ofPattern("MMMM", Locale.ENGLISH));
+            Reward reward = rewardsByCustId.get(i);
+            if(monthMap.containsKey(month))
+            {
+                RewardDetail rewardDetail = monthMap.get(month);
+                List<TransactionAndPoints> transactionAndPointsList = rewardDetail.getTransactionAndPoints();
+                transactionAndPointsList.add(new TransactionAndPoints(tran.getTranDate(), tran.getAmount(), reward.getPoints()));
+                rewardDetail.setTransactionAndPoints(transactionAndPointsList);
+                rewardDetail.setAmountSpent(rewardDetail.getAmountSpent()+ tran.getAmount());
+                rewardDetail.setPoints(rewardDetail.getPoints()+reward.getPoints());
+                monthMap.put(month, rewardDetail);
+            }
+            else{
+                RewardDetail rewardDetail = new RewardDetail(month, reward.getPoints(), tran.getAmount());
+                List<TransactionAndPoints> transactionAndPointsList = new ArrayList<>();
+                transactionAndPointsList.add(new TransactionAndPoints(tran.getTranDate(), tran.getAmount(), reward.getPoints()));
+                rewardDetail.setTransactionAndPoints(transactionAndPointsList);
+                monthMap.put(month, rewardDetail);
+            }
+            for(String m : monthMap.keySet()){
+                rewardDetails.add(monthMap.get(m));
+            }
+            Collections.sort(rewardDetails,(j,k)-> monthSort(j.getMonth())>monthSort(k.getMonth())?1:-1);
+            rewardResponse.setRewardDetails(rewardDetails);
+            rewardResponse.setTotalAmountSpent(rewardDetails.stream().mapToInt(RewardDetail::getAmountSpent).sum());
+            rewardResponse.setTotal_points(rewardDetails.stream().mapToInt(RewardDetail::getPoints).sum());
+            rewardDetails = new ArrayList<>();
         }
-        Collections.sort(rewardDetails,(i,j)-> monthSort(i.getMonth())>monthSort(j.getMonth())?1:-1);
-        rewardResponse.setRewardDetails(rewardDetails);
-        rewardResponse.setTotal_points(totalPoints);
-        rewardResponse.setTotalAmountSpent(totalAmountSpent);
-        return new ResponseEntity<>(rewardResponse, HttpStatus.OK);
+        return rewardResponse;
     }
 
     private int monthSort(String month){
@@ -137,4 +117,5 @@ public class RewardsServiceImpl implements RewardsService {
         else if (amount <= 100) return (amount - 50);
         else return 2 * (amount - 100) + 50;
     }
+
 }
