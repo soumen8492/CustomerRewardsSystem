2ba8c4c18bdc462e69957176624170914f84b811
SOUMEN MITRA <soumen8492@gmail.com>
2025-01-31 21:55:48 +0530

    Repositories updated with Queries of fetch and joins to organise data

--- src/main/java/CustomerRewardsApp/customerRewardsRepository/TransactionRepository.java
+++ src/main/java/CustomerRewardsApp/customerRewardsRepository/TransactionRepository.java
@@ -2,8 +2,17 @@ package CustomerRewardsApp.customerRewardsRepository;
 
 import CustomerRewardsApp.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
+    @Query(value = "select distinct cust_id from CUSTOMERTRANSACTION order by cust_id asc", nativeQuery = true)
+    List<String> findCustId();
+
+    @Query(value = "select * from CUSTOMERTRANSACTION WHERE cust_id =:custId ORDER BY tran_id", nativeQuery = true)
+    List<Transaction> findTransactionsByCustId(@Param("custId") String custId);
 }
